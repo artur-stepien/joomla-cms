ff1fd3af6764399e1feb7cf7d28b34143ef21e92
Artur Stępień <webdesign@b-support.pl>
2014-06-26 16:07:24 +0200

    Added marking of current item for aliases
    This will fix the problem with marking aliases to current page. Before this, if there are two links to one menu item in menu (on to component and one alias to that item) only one is marked as current. This is wrong, both of them point to the same position so both should be marked as current.

--- modules/mod_menu/tmpl/default.php
+++ modules/mod_menu/tmpl/default.php
@@ -23,7 +23,7 @@ defined('_JEXEC') or die;
 <?php
 foreach ($list as $i => &$item) :
 	$class = 'item-'.$item->id;
-	if ($item->id == $active_id)
+	if (($item->id == $active_id) OR ($item->type=='alias' AND $item->params->get('aliasoptions')==$active_id))
 	{
 		$class .= ' current';
 	}
